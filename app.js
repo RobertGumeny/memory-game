d081b702b4b6fe59f516da2159e2af6e9baa5741
Robert Gumeny <robert.gumeny528@gmail.com>
2020-08-31 23:56:49 -0600

    game board is drawn

--- app.js
+++ app.js
@@ -1,5 +1,69 @@
 document.addEventListener("DOMContentLoaded", () => {
   // Options go here
 
-  const cardArray = [];
+  const cardArray = [
+    {
+      name: "Cheese",
+      img: "images/cheese-wedge.png",
+    },
+    {
+      name: "Cheese",
+      img: "images/cheese-wedge.png",
+    },
+    {
+      name: "Carrot",
+      img: "images/carrot.png",
+    },
+    {
+      name: "Carrot",
+      img: "images/carrot.png",
+    },
+    {
+      name: "Chili",
+      img: "images/chili-pepper.png",
+    },
+    {
+      name: "Chili",
+      img: "images/chili-pepper.png",
+    },
+    {
+      name: "Apple",
+      img: "images/shiny-apple.png",
+    },
+    {
+      name: "Apple",
+      img: "images/shiny-apple.png",
+    },
+    {
+      name: "Grapes",
+      img: "images/grapes.png",
+    },
+    {
+      name: "Grapes",
+      img: "images/grapes.png",
+    },
+    {
+      name: "Bread",
+      img: "images/sliced-bread.png",
+    },
+    {
+      name: "Bread",
+      img: "images/sliced-bread.png",
+    },
+  ];
+
+  const grid = document.querySelector(".grid");
+
+  // Create game board
+  function createBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
+      let card = document.createElement("img");
+      card.setAttribute("src", "images/blank.png");
+      card.setAttribute("data-id", i);
+      // card.addEventListener('click', flipcard);
+      grid.appendChild(card);
+    }
+  }
+
+  createBoard();
 });
